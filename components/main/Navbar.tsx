e18a3ddad91e99ae63aa86193eb73c98c4b6148f
Hamza3re <meriminatia@gmail.com>
2025-02-09 23:30:56 +0500

    second commit

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                     />
 
                     <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-                        WebChain Dev
+                        Hamza Khan
                     </span>
                 </a>
                 <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,19 +12,19 @@ const Projects = () => {
             </h1>
             <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
                 <ProjectCard
-                    src="/NextWebsite.png"
+                    src="/SpaceWebsite.png"
                     title="Modern Next.js Portfolio"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                 />
                 <ProjectCard
-                    src="/CardImage.png"
-                    title="Interactive Website Cards"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    src="/E-commerce.png"
+                    title="Fully Functional E-Commerce Website"
+                    description="A E-Commerce Website With Some Functionalities Like Add To Card ."
                 />
                 <ProjectCard
-                    src="/SpaceWebsite.png"
-                    title="Space Themed Website"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    src="/resume.png"
+                    title="Resume Builder"
+                    description="By Using Simple TypeScript and HTML and CSS."
                 />
             </div>
         </div>
